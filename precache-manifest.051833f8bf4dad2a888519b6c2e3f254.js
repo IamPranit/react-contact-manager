9132e868d6451215682cdb8dd4164c2ef624193f
IamPranit <maskepranit@gmail.com>
2020-03-28 22:18:43 +0530

    Updates

---
+++ precache-manifest.051833f8bf4dad2a888519b6c2e3f254.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f13173383b512cb51a1f2c1660932d",
+    "url": "/reactContactManager/index.html"
+  },
+  {
+    "revision": "8b2dd02335d46e7866eb",
+    "url": "/reactContactManager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bdb94557bd081343c43d",
+    "url": "/reactContactManager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8b2dd02335d46e7866eb",
+    "url": "/reactContactManager/static/js/2.f83aa4ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactContactManager/static/js/2.f83aa4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb94557bd081343c43d",
+    "url": "/reactContactManager/static/js/main.b6d68c07.chunk.js"
+  },
+  {
+    "revision": "a2a4a1c6a65faef42011",
+    "url": "/reactContactManager/static/js/runtime-main.95e0f8f2.js"
+  }
+]);
